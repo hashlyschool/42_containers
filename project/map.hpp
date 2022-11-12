b42a7d066b2881569e8c48703269c977494e9654
hashly <ilya12-141998@mail.ru>
2022-11-12 23:35:22 +0300

    fix error max_size

--- project/map.hpp
+++ project/map.hpp
@@ -482,7 +482,7 @@ private:
 template <class Key, class T, class Compare, class Alloc>
 const typename map<Key, T, Compare, Alloc>::size_type
 map<Key, T, Compare, Alloc>::_max_size =
-	std::numeric_limits<difference_type>::max() / (sizeof(node_type) / 2 ?: 1);
+	std::numeric_limits<difference_type>::max() / sizeof(node_type);
 
 template<class Key, class T, class Compare, class Alloc>
 bool	operator==(const map<Key, T, Compare, Alloc> &lhs, const map<Key, T, Compare, Alloc> &rhs)
