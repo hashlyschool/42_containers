b82197abd11ead9f75c21a9b8854f2754454b777
hashly <ilya12-141998@mail.ru>
2022-11-13 21:22:13 +0300

    fix error

--- project/iterators/RandomAccessIterator.hpp
+++ project/iterators/RandomAccessIterator.hpp
@@ -21,7 +21,7 @@ class RandomAccessIterator : ft::Iterator<ft::random_access_iterator_tag, T>
 	public:
 		typedef typename ft::Iterator<ft::random_access_iterator_tag, T>::iterator_category	iterator_category;
 
-		// typedef typename ft::Iterator<ft::random_access_iterator_tag, T>::value_type	value_type;
+		typedef T	value_type;
 
 		typedef typename ft::Iterator<ft::random_access_iterator_tag, T>::difference_type	difference_type;
 
